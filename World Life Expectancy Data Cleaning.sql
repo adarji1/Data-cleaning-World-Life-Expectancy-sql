c450238e3f5a9291e933f6edc0856a0b50a14bea
Aryan Darji <163683712+adarji1@users.noreply.github.com>
2026-02-03 18:03:40 -0500

    Clean up SQL script and remove duplicates
    Removed duplicate rows and cleaned up the SQL script by eliminating unnecessary update statements. Added comments for clarity on data cleaning steps.

--- World Life Expectancy Data Cleaning.sql
+++ World Life Expectancy Data Cleaning.sql
@@ -1,12 +1,16 @@
 # WORLD LIFE EXPECTANCY PROJECT (DATA CLEANING)
 
+
 SELECT * FROM world_life_expectancy;
+
+-- Which country–year combinations appear more than once in the world life expectancy data?
 
 SELECT country, year, concat(country, year), count(concat(country, year))
 FROM world_life_expectancy
 GROUP BY  country, year, concat(country, year)
 HAVING count(concat(country, year)) > 1;
 
+-- Which rows should be removed to keep only one record per country per year?
 SELECT *
 FROM (
 		SELECT ROW_ID, concat(country, year),
@@ -14,6 +18,7 @@ FROM (
 		FROM world_life_expectancy) AS ROW_TABLE
 WHERE ROW_NUM > 1;
 
+--Remove all duplicate rows for the same country–year combination, keeping only the first occurrence.
 
 DELETE FROM world_life_expectancy
 WHERE 
@@ -27,50 +32,16 @@ WHERE ROW_NUM > 1
 )
 ;
 
-
+-- What are the unique development statuses in the dataset?
 SELECT DISTINCT(Status)
 FROM world_life_expectancy;
 
+--Which countries in the dataset are classified as Developing?
 select DISTINCT(Country)
 from world_life_expectancy
 WHERE Status = 'Developing';
 
-UPDATE world_life_expectancy
-SET Status = 'Developing'
-WHERE Country IN (
-select DISTINCT(Country)
-from world_life_expectancy
-WHERE Status = 'Developing');  # sub queries do not work in update table 
-
-UPDATE world_life_expectancy t1
-JOIN world_life_expectancy t2
-	ON t1.Country = t2.Country
-SET t1.Status = 'Developing'
-WHERE t1.Status = ''
-AND t2.Status <> ''
-AND t2.Status = 'Developing'
-;
-
-UPDATE world_life_expectancy t1
-JOIN world_life_expectancy t2
-	ON t1.Country = t2.Country
-SET t1.Status = 'Developed'
-WHERE t1.Status = ''
-AND t2.Status <> ''
-AND t2.Status = 'Developed'
-;
-
-select DISTINCT(country)
-from world_life_expectancy
-WHERE Status = '';
-
-SELECT *
-from world_life_expectancy;
-
-SELECT country, `Life expectancy`
-from world_life_expectancy
-WHERE `Life expectancy` = '';
-
+--For countries and years where Life expectancy is missing, what are the life expectancy values in the previous and next years, so we can estimate or impute the missing value?
 SELECT t1.country, t1.year, t1.`Life expectancy`,
 t2.Country, t2.year, t2.`Life expectancy`,
 t3.Country, t3.year, t3.`Life expectancy`,
@@ -83,13 +54,3 @@ JOIN world_life_expectancy t3
 	ON t1.country = t3.country
     AND t1.year = t3.year + 1
 WHERE t1.`Life expectancy` = ''; 
-
-UPDATE world_life_expectancy t1
-JOIN world_life_expectancy t2
-	ON t1.country = t2.country
-    AND t1.year = t2.year - 1
-JOIN world_life_expectancy t3
-	ON t1.country = t3.country
-    AND t1.year = t3.year + 1
-SET t1.`Life expectancy` = ROUND((t2.`Life expectancy` + t3.`Life expectancy`)/2,1)
-WHERE t1.`Life expectancy` = '';
