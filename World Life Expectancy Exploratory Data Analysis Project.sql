9ec77103d51660d17ef2a469f383cfbdb650d04c
Aryan Darji <163683712+adarji1@users.noreply.github.com>
2026-02-03 14:40:50 -0500

    Enhance SQL for life expectancy exploratory analysis
    Added SQL queries for exploratory data analysis on life expectancy, including data pulls, calculations for life expectancy increases, GDP comparisons, and averages by development status.

--- World Life Expectancy Exploratory Data Analysis Project.sql
+++ World Life Expectancy Exploratory Data Analysis Project.sql
@@ -1,7 +1,11 @@
 # World Life Expectancy (EXPLORATORY DATA ANALYSIS)
-
+  
+-- Pull the Data
+  
 SELECT *
 FROM world_life_expectancy;	
+
+-- What is the minimum and maximum life expectancy recorded for each country, and what is the total increase over time?
 
 SELECT country, 
 min(`Life expectancy`), 
@@ -22,6 +26,8 @@ AND `Life expectancy` <> 0
 GROUP BY year
 ORDER BY year;
 
+-- How does life expectancy differ between high-GDP and low-GDP countries?
+
 SELECT
 SUM(CASE WHEN GDP >=1500 THEN 1 ELSE 0 END) HIGH_GDP_COUNT,
 AVG(CASE WHEN GDP >=1500 THEN `Life expectancy` ELSE NULL END) HIGH_GDP_Life_expectancy,
@@ -30,13 +36,19 @@ AVG(CASE WHEN GDP <=1500 THEN `Life expectancy` ELSE NULL END) LOW_GDP_Life_expe
 FROM world_life_expectancy
 ;	
 
-SELECT status, round(avg(`Life expectancy`),1)
-FROM world_life_expectancy
-Group by status;
+-- What is the average life expectancy by development status?
+
+  SELECT status, round(avg(`Life expectancy`),1)
+  FROM world_life_expectancy
+  Group by status;
+
+--For each development status, how many unique countries are there, and what is their average life expectancy?
 
 SELECT status, count(distinct country), round(avg(`Life expectancy`),1)
 FROM world_life_expectancy
 Group by status;
+
+--What is the average life expectancy and average BMI for each country, and which countries have the highest average BMI?
 
 SELECT country, ROUND(AVG(`Life expectancy`),1) AS Life_Exp, ROUND(AVG(BMI),1) AS BMI
 FROM world_life_expectancy
@@ -45,6 +57,8 @@ HAVING Life_Exp > 0
 AND BMI > 0
 ORDER BY BMI DESC
 ;
+
+--What is the year-by-year adult mortality trend for ‘United’ countries, and how does it accumulate over time?
 
 SELECT country,
 year,
